df9601bb8c4a8d4714b3e71037d72b5ddec231de
MikeBrownse <2534701930@qq.com>
2020-05-02 17:00:21 +0800

    first commit

---
+++ src/windowSys/Login.java
@@ -0,0 +1,165 @@
+package windowSys;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+import windowSys.LoginError.LoginErrorCode;
+
+class LoginError extends JDialog{		//用户名不正确时弹出的对话框类, finished class
+	
+	class LoginErrorCode{				//关于用户名的错误码, finished inner class
+		final static int NoCard = 0;
+		final static int Unregistered = 1;
+		final static int Registered = 2;
+		final static int InvaildUserName = 3;
+		final static int WrongPsw = 4;
+	}
+	
+	public LoginError(Login LoginWin, int ErrorCode) {
+		super(LoginWin, "登录错误", true);
+		JPanel jp = new JPanel(new BorderLayout());
+		switch(ErrorCode) {
+			case LoginErrorCode.NoCard : 			jp.add(new JLabel("请刷卡！", JLabel.CENTER), BorderLayout.CENTER);		break;
+			case LoginErrorCode.Unregistered : 		jp.add(new JLabel("用户未注册！", JLabel.CENTER), BorderLayout.CENTER);		break;
+			case LoginErrorCode.Registered : 		jp.add(new JLabel("该用户名已被注册！", JLabel.CENTER), BorderLayout.CENTER);	break;
+			case LoginErrorCode.InvaildUserName : 	jp.add(new JLabel("无效用户名！", JLabel.CENTER), BorderLayout.CENTER);		break;
+			case LoginErrorCode.WrongPsw : 			jp.add(new JLabel("密码错误！", JLabel.CENTER), BorderLayout.CENTER);		break;
+			default : 								jp.add(new JLabel("未知错误！", JLabel.CENTER), BorderLayout.CENTER);		break;
+		}
+
+		getContentPane().add(jp);
+		setSize(300, 200);
+		setLocationRelativeTo(null);
+	}
+}
+
+
+
+public class Login extends JFrame {
+
+	public boolean StringIsDig(String s) {		//判断字符串是否均为数字, finished method
+		boolean result = true;
+		
+		for(int i = 0; i < s.length(); i++) {
+			if(!Character.isDigit(s.charAt(i))) {
+				result = false;
+				break;
+			}
+		}
+		
+		return result;
+	}
+	
+	boolean EqualToDB(String acc) {			//数据库对比用户名, To be continued method
+		boolean result = false;
+		
+		return result;
+	}
+	
+	public Login() {						//登录模块的具体实现, To be continued construct method
+		Container container = getContentPane();
+		container.setLayout(new GridLayout(4, 1));		//创建4行1列网格布局
+		
+		JPanel p1 = new JPanel(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		JPanel p2 = new JPanel(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		JPanel p3 = new JPanel(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		JPanel p4 = new JPanel(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		final JTextField UserName = new JTextField("请刷卡", 10);
+		final JTextField Password = new JTextField("请输入密码", 10);
+		final JButton LoginBtn = new JButton("登录");
+		final JButton RegBtn = new JButton("注册");
+		final JButton ResetBtn = new JButton("重置");
+		UserName.setEditable(false);		//禁止用户手动输入用户名
+		
+		LoginBtn.addActionListener(new ActionListener() {		//登录按钮监视器
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO 自动生成的方法存根
+				if(UserName.getText().equals("请刷卡")) {
+					new LoginError(Login.this, LoginErrorCode.NoCard).setVisible(true);
+				}else if(StringIsDig(UserName.getText())){
+					if(EqualToDB(UserName.getText())) {			//对比数据库
+						
+						/**************************************进入选座界面, To be continued*********************************/
+						
+						System.out.println("login");
+						System.out.println(UserName.getText());
+						System.out.println(Password.getText());
+						
+						/**************************************调用结束*************************************/
+						
+					}else {
+						new LoginError(Login.this, LoginErrorCode.Unregistered).setVisible(true);
+					}
+					
+				}else {
+					new LoginError(Login.this, LoginErrorCode.InvaildUserName).setVisible(true);
+				}
+			}
+		});
+		
+		RegBtn.addActionListener(new ActionListener() {			//注册按钮监视器
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO 自动生成的方法存根				
+				if(UserName.getText().equals("请刷卡")) {
+					new LoginError(Login.this, LoginErrorCode.NoCard).setVisible(true);
+				}else if(StringIsDig(UserName.getText())){
+					if(!EqualToDB(UserName.getText())) {			//对比数据库
+						
+						/**************************************写入数据库, To be continued*********************************/
+						
+						System.out.println("registration");
+						System.out.println(UserName.getText());
+						System.out.println(Password.getText());
+						
+						/**************************************调用结束*************************************/
+						
+					}else {
+						new LoginError(Login.this, LoginErrorCode.Registered).setVisible(true);
+					}
+					
+				}else {
+					new LoginError(Login.this, LoginErrorCode.InvaildUserName).setVisible(true);
+				}
+				
+			}
+		});
+		
+		ResetBtn.addActionListener(new ActionListener() {		//重置按钮监视器, finished
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO 自动生成的方法存根
+				UserName.setText("请刷卡");
+				Password.setText("");
+				Password.requestFocus();
+			}
+		});
+		
+		p1.add(new JLabel("欢迎使用图书馆座位预约系统，请刷卡"));
+		p2.add(new JLabel("卡号"));
+		p2.add(UserName);
+		p3.add(new JLabel("密码"));
+		p3.add(Password);
+		p4.add(LoginBtn);
+		p4.add(RegBtn);
+		p4.add(ResetBtn);
+		
+		container.add(p1);
+		container.add(p2);
+		container.add(p3);
+		container.add(p4);
+		
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setTitle("图书馆座位预约系统");
+		setSize(600, 300);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		
+	}
+}
---
+++ src/windowSys/MainProceed.java
@@ -0,0 +1,10 @@
+package windowSys;
+
+public class MainProceed {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		new Login();
+	}
+
+}
